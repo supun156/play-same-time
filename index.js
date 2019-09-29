e6394711e55cb7bb902324f445326ea666dc5fec
supun156 <supun@smallaxe.tech>
2019-09-29 20:39:38 +0530

    ADD JS FILE

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 5000
-app.use(express.static('public'));
-app.get('/', (req, res) => res.send('Hello World!'))
+const express = require("express");
+const app = express();
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+
+const port = 5000;
+app.use(express.static("public"));
+app.get("/", (req, res) => res.send("Hello World!"));
+
+server.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
+
+io.on("connection", function(socket) {
+
+    socket.on('join_room', function(data){
+
+        console.log(data);
+
+        socket.join(data.room);
+     });
+
+
+
+     socket.on('playSong', function(data){
+
+
+
+        console.log(data);
+
+     //socket.to(data.room).emit('playSoundNow', "let's play a game");
+     socket.emit('broadcast', 'hello friends!');
+     });
+
+
+
+});
--- public/js/wavesurfer.js
+++ public/js/wavesurfer.js
@@ -1,18 +1,25 @@
+var socket = io.connect("https://play-at-same-time.herokuapp.com");
+var roomName = "test";
+
+socket.emit("join_room", { room: roomName });
+
+//socket.to(roomName).emit('some event');
+
 var wavesurfer = WaveSurfer.create({
   container: "#waveform",
   waveColor: "violet",
   barWidth: 2,
-  barHeight:100,
-  cursorWidth:2,
-  height:200,
-  hideScrollbar:true,
+  barHeight: 100,
+  cursorWidth: 2,
+  height: 200,
+  hideScrollbar: true,
   progressColor: "purple"
 });
-wavesurfer.on('ready', function () {
+wavesurfer.on("ready", function() {
   var timeline = Object.create(WaveSurfer.Timeline);
   timeline.init({
     wavesurfer: wavesurfer,
-    container: '#waveform-timeline'
+    container: "#waveform-timeline"
   });
 });
 wavesurfer.load("assets/1.mp3");
@@ -104,63 +111,13 @@ wavesurfer.on("ready", function() {
   // For debugging
   wavesurfer.filters = filters;
 });
-/*var myVar = setInterval(myTimer, 1000);
-var timeGet = 0;
-function myTimer() {
-  if (wavesurfer.isPlaying()) {
-    // console.log('song paly start');
-    //  console.log(wavesurfer.getCurrentTime());
-    //timeGet =  wavesurfer.getCurrentTime();
-  }
-  // console.log(wavesurfer.isPlaying());
-}*/
-var key = [];
-function addKeyToarray(t, r) {
-    var subKey = {};
-    subKey["time"] = t;
-    subKey["type"] = r;
-  key.push(subKey);
-}
-//addKeyToarray(0, 0);
-$(document).on("keydown", function(event) {
-  //$( "#log" ).html( event.type + ": " +  event.which );
-  // console.log(event.which);
-  if (wavesurfer.isPlaying()) {
-    var  sonKeyTime = Math.trunc(wavesurfer.getCurrentTime()*1000);
-    var red = '<div class=" box"></div>';
-    var blue = '<div class="  box"></div>';
-    var blueandRed = "";
-    if (event.which == 191) {
-      red = '<div class="  bg-danger box"></div>';
-      addKeyToarray(sonKeyTime,1);
-    }
-    if (event.which == 32) {
-      blueandRed =
-        '<div class=" bg-danger box"></div><div class=" bg-primary box"></div>';
-      addKeyToarray(sonKeyTime, 1);
-      addKeyToarray(sonKeyTime, 0);
-    }
-    if (event.which == 90) {
-      blue = '<div class=" bg-primary box"></div>';
-      addKeyToarray(sonKeyTime, 0);
-      //console.log(wavesurfer.getCurrentTime());
-    }
-    if (event.which == 90 || event.which == 32 || event.which == 191) {
-      $("#boxArray").scrollTop($("#boxArray")[0].scrollHeight);
-      $("#boxArray").append(
-        '<div class="row boxRow"><div class="col col-sm-4">' +
-           sonKeyTime+
-          "</div>" +
-          blueandRed +
-          red +
-          blue
-      );
-    }
-  }
+$("#play_now").click(function(r){
+  //
+
+  socket.emit("playSong", { room: roomName });
 });
-function getJson() {
-  var myJsonString = JSON.stringify(key);
-  $("#jsonOut").val(myJsonString);
-  $("#jsonOut").show();
-  //console.log(myJsonString);
-}
+
+socket.on('broadcast', function(data){
+  wavesurfer.playPause();
+ });
+
